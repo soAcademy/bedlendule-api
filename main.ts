5efb8b0fabd2c976a5c33264a24d4776263c8241
jaiieth <midzty.rb128@gmail.com>
2023-03-30 15:48:04 +0700

    open cors

--- main.ts
+++ main.ts
@@ -3,19 +3,14 @@ import { AppRoutes } from "./src";
 import cors from "cors";
 const app = express();
 app.use(express.json());
-app.use(
-  cors()
-  //   {
-  //   origin: ["http://localhost:3000"],
-  // }
-);
+app.use(cors());
 
 AppRoutes.map((route) => {
   app[route.method as keyof Application](
     route.path,
     (req: Request, res: Response, next: NextFunction) => {
       route.middleware && route.middleware(req, res, next);
-      return next()
+      return next();
     },
     (req: Request, res: Response, next: NextFunction) =>
       route.action(req, res, next)
